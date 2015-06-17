220710278a4ce793fe4ba8d5f5662caf35e36291
Tim Dunn <timdunn2@gmail.com>
2015-06-17 16:54:32 -0700

    email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,18 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "mail.google.com",
+  :user_name => "my@gmail.com",
+  :password => "mypassword",
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
